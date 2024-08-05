7067253d17a773aac56eb2a4f1d29253ee2414f5
Cano Shih <y781025@gmail.com>
2024-08-05 10:46:17 +0800

    git init

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        num: <span>{{ num }}</span>
+        <br>
+        <button v-on:click="numPlusOne">按一次加 1</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+    let num = ref(1);
+
+    function numPlusOne(){
+        num.value++
+    }
+</script>
+
+<style scoped></style>
